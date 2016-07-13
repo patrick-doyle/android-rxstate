fcb9a7016f850f496b8007e6b458d7a892846519
twistedequations <patrick@twistedequations.com>
2016-07-13 23:16:22 +0100

    Fixed string typo

--- rx-savestate/src/main/java/com/twistedequations/rxstate/internal/PreConditions.java
+++ rx-savestate/src/main/java/com/twistedequations/rxstate/internal/PreConditions.java
@@ -7,7 +7,7 @@ public class PreConditions {
 
     public static void throwIfNotOnMainThread() {
         if(Looper.myLooper() != Looper.getMainLooper()) {
-            throw new IllegalArgumentException("Cant call RxSaveState create a background thread");
+            throw new IllegalArgumentException("Cant call RxSaveState from a background thread, move it to the main thread");
         }
     }
 }
